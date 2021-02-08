392b4c438567c63247e614dad53c735b6d96dd11
Carlos Ortiz <cortizmardones@gmail.com>
2021-02-08 16:28:38 -0300

    se agrega libreria fontawesome por npm para iconos en las tablas html5

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Cliente } from './cliente';
+
+//Icono libreria FontAwesome
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
+import { faCity } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-clientes',
@@ -8,12 +14,19 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent  {
 
+  //Juego de iconos fontawesome para llenar la tabla del componente
+  faAddressBook = faAddressBook;
+  faMailBulk = faMailBulk;
+  faCalendarAlt = faCalendarAlt;
+  faCity = faCity ;
+
   Clientes: Cliente [] = [
-    {id: 1 , nombre : 'Esteban' , apellido : 'Bustos' , email : 'ebustos@gmail.com' , createAt : '1991-07-24'},
-    {id: 2 , nombre : 'Carlos' , apellido : 'Ortiz' , email : 'cortizmardones@gmail.com' , createAt : '1988-03-22'},
-    {id: 3 , nombre : 'Marcelo' , apellido : 'Veloso' , email : 'mveloso@gmail.com' , createAt : '1993-12-09'},
-    {id: 4 , nombre : 'Francisco' , apellido : 'Cancino' , email : 'fcancino@gmail.com' , createAt : '1985-04-03'},
-    {id: 5 , nombre : 'Jose' , apellido : 'bernales' , email : 'jbernales@gmail.com' , createAt : '2017-12-11'}
+    {id: 1 , nombre : 'Esteban' , apellido : 'Bustos' , email : 'ebustos@gmail.com' , createAt : '1991-07-24' , comuna : 'San Bernardo'},
+    {id: 2 , nombre : 'Carlos' , apellido : 'Ortiz' , email : 'cortizmardones@gmail.com' , createAt : '1988-03-22', comuna : 'Puente Alto'},
+    {id: 3 , nombre : 'Marcelo' , apellido : 'Veloso' , email : 'mveloso@gmail.com' , createAt : '1993-12-09', comuna : 'Santiago'},
+    {id: 4 , nombre : 'Francisco' , apellido : 'Cancino' , email : 'fcancino@gmail.com' , createAt : '1985-04-03', comuna : 'Puente Alto'},
+    {id: 5 , nombre : 'Jose' , apellido : 'bernales' , email : 'jbernales@gmail.com' , createAt : '2017-12-11', comuna : 'Coñaripe'},
+    {id: 6 , nombre : 'Ricardo' , apellido : 'Soto' , email : 'rsoto@gmail.com' , createAt : '1993-11-03', comuna : 'Colina 2'}
   ];
 
   public tabla: boolean = true;
@@ -22,9 +35,8 @@ export class ClientesComponent  {
 
   ocultarTabla() : void {
     this.tabla = (this.tabla==true) ? false : true;
-    this.textoBoton = (this.tabla==true) ? "Ocultar Tabla" : "Mostrar Tabla"; 
-    this.tituloTabla = (this.tabla==true) ? "Listado de clientes" : "Tabla vacía"; 
+    this.textoBoton = (this.tabla==true) ? "Ocultar Tabla" : "Mostrar Tabla";
+    this.tituloTabla = (this.tabla==true) ? "Listado de clientes" : "Tabla vacía";
   }
 
 }
-
