20fd94494876b430a49c983c6114196474706f06
Carlos Ortiz <cortizmardones@gmail.com>
2021-02-09 10:31:34 -0300

    se agrega la funcion editar usuario pero falta utilizar formularios para actualizar los datos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -9,6 +9,7 @@ import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-clientes',
@@ -25,7 +26,9 @@ export class ClientesComponent  {
   faCity = faCity ;
   faEdit = faEdit;
   faTrashAlt = faTrashAlt;
+  faUserPlus = faUserPlus;
 
+//Arreglo para llenar con los datos la tabla
   public Clientes : Cliente [] = [
     {id: 1 , nombre : 'Esteban' , apellido : 'Bustos' , email : 'ebustos@gmail.com' , createAt : '1991-07-24' , comuna : 'San Bernardo'},
     {id: 2 , nombre : 'Carlos' , apellido : 'Ortiz' , email : 'cortizmardones@gmail.com' , createAt : '1988-03-22', comuna : 'Puente Alto'},
@@ -36,23 +39,52 @@ export class ClientesComponent  {
     {id: 7 , nombre : 'Elias' , apellido : 'Sanchez' , email : 'esanchez@gmail.com' , createAt : '1993-04-15', comuna : 'Maipú'}
   ];
 
+//Objeto vacio para agregarlo al editar
+  public objetoCliente : Cliente = {id:50,nombre:'',apellido:'',email:'',createAt:'',comuna:''};
+
+  //Campos para las interaciones de la tabla
   public tabla: boolean = true;
   public textoBoton: string ="Ocultar Tabla";
   public tituloTabla: string = "Listado de clientes";
 
+  //Campo para las interacciones de la seccion editar
+  public divEdit: boolean = false;
+  public nombre : string = '';
+  public apellido : string = '';
+  public email : string = '';
+  public createAt : string = '';
+  public comuna : string = '';
+
+  //Funciones para la interactividad
   ocultarTabla() : void {
     this.tabla = (this.tabla==true) ? false : true;
     this.textoBoton = (this.tabla==true) ? "Ocultar Tabla" : "Mostrar Tabla";
     this.tituloTabla = (this.tabla==true) ? "Listado de clientes" : "Tabla vacía";
   }
 
-  editar( nombre: string , apellido : string ) : void {
+  editar( nombre: string , apellido : string , email : string , createAt : string , comuna : string) : void {
     console.log("Editando a : " + nombre + " " + apellido);
+    this.divEdit = true;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.createAt = createAt;
+    this.comuna = comuna;
   }
 
   eliminar( position: number) : void {
     console.log("Eliminar...");
     this.Clientes.splice( position , 1 );
+  }
+
+  guardarCambiosEditar(nombre: string , apellido : string , email : string , createAt : string , comuna : string) : void {
+    console.log("Guardando cambios");
+    this.objetoCliente.nombre = nombre;
+    this.objetoCliente.apellido = apellido;
+    this.objetoCliente.email = email;
+    this.objetoCliente.createAt = createAt;
+    this.objetoCliente.comuna = comuna;
+    this.Clientes.push(this.objetoCliente);
   }
 
 }
